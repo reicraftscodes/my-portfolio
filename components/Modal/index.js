ae8a25eef9e5fae9c8afd633afb472b9fd550431
laureisan <reicraftscodes@gmail.com>
2024-01-29 13:19:24 +0000

    style: add top and bottom borderline on modal

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -35,15 +35,17 @@ const ProjectModal = ({ project, onClose }) => {
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
       <div  ref={modalRef} className="bg-white dark:bg-slate-800 rounded-lg p-8 max-w-2xl mx-auto relative dark:text-white ring-1 ring-slate-900/5 shadow-xl">
-        <h2 className="text-lg font-bold mb-4">{project.title}</h2>
+        <h2 className="text-lg font-bold mb-4 ">{project.title}</h2>
+        <div class="border-b dark:border-gray-600"> </div>
         {/* Apply vertical scrolling to the modal content */}
         <div className="max-h-80 overflow-y-auto">
           {/* Render fullDesc as HTML with line breaks and bullet points */}
           <div dangerouslySetInnerHTML={{ __html: project.fullDesc }}></div>
         </div>
+        <div class="border-t dark:border-gray-600"> </div>
          {/* Close button */}
          <button
-          className="mt-4 px-4 py-2 bg-gray-500 text-white rounded-md cursor-pointer"
+          className="mt-4 px-4 py-2 bg-gray-500 hover:bg-gray-700  text-white rounded-md cursor-pointer"
           onClick={onClose}
         >
           Close
