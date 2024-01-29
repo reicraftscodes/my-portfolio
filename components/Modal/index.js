dc1d7b3d0bdc43ce50f13303d0c4d8980ed21193
laureisan <reicraftscodes@gmail.com>
2024-01-29 00:46:44 +0000

    refactor: code clean up, removed exit button and implemented automatically closing the modal when clicking outside of it

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
+const useModalInteraction = (onClose) => {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    const handleEscapeKey = (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleEscapeKey);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [onClose]);
+  
+  return modalRef;
+};
 
 const ProjectModal = ({ project, onClose }) => {
-
   
+  const modalRef = useModalInteraction(onClose);
+
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
-      <div className="bg-white dark:bg-slate-800 rounded-lg p-8 max-w-2xl mx-auto relative dark:text-white ring-1 ring-slate-900/5 shadow-xl">
-        <button className="absolute top-4 right-4 text-gray-500" onClick={onClose}>
-          <span className="sr-only">Close</span>
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-          </svg>
-        </button>
+      <div  ref={modalRef} className="bg-white dark:bg-slate-800 rounded-lg p-8 max-w-2xl mx-auto relative dark:text-white ring-1 ring-slate-900/5 shadow-xl">
         <h2 className="text-lg font-bold mb-4">{project.title}</h2>
         {/* Apply vertical scrolling to the modal content */}
         <div className="max-h-80 overflow-y-auto">
@@ -36,4 +52,5 @@ const ProjectModal = ({ project, onClose }) => {
     </div>
   );
 };
+
 export default ProjectModal;
