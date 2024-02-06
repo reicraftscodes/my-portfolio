b0c72bec69e54d0096f73727f3785a75693aa61b
laureisan <reicraftscodes@gmail.com>
2024-02-06 15:41:50 +0000

    refactor:adjusted blog page height and weigth

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -20,18 +20,19 @@ const BlogPost = () => {
     }
 
     return (
-        <Container>
-            <div>
-                <Spacer />
-                <Spacer />
-                <section className="w-full flex items-center justify-center">
-                    <div>
-                        <h1 className="font-bold text-3xl text-center">{blogPost.title}</h1>
-                        <p className="text-center">{blogPost.content}</p>
-                    </div>
-                </section>
+        <div className="transition duration-300 ease-in p-3 flex justify-center h-full w-full">
+            <div className="lg:w-[60%]">
+                <main className='transition duration-300 ease-in p-3 flex justify-center w-full'>
+                    <section className="w-full flex items-center justify-center ">
+                        <div>
+                            <h1 className="font-bold text-3xl text-center p-3">{blogPost.title}</h1>
+                            <p className="text-center">{blogPost.content}</p>
+                        </div>
+                    </section>
+                </main>
             </div>
-        </Container>
+        </div>
+
     );
 };
 
