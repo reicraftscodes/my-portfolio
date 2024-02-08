bc6d69028886da4cf9c670704d32900523a8bd62
laureisan <reicraftscodes@gmail.com>
2024-02-08 22:59:06 +0000

    style: fixed syntax highlight

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,21 +4,64 @@ import fs from 'fs';
 import path from 'path';
 import { MDXRemote } from 'next-mdx-remote';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Spacer from '../../components/Spacer';
 import ContainerBlog from '../../components/ContainerBlog';
 import { MDXProvider } from '@mdx-js/react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { nightOwl } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 const components = {
   h1: (props) => <h1 style={{ fontSize: '2em', fontWeight: 'bold' }} {...props} />,
   h2: (props) => <h2 style={{ fontSize: '1em', fontWeight: 'bold' }} {...props} />,
+  pre: ({ children }) => <>{children}</>, // Use a wrapper for the pre tag
+  code: ({ className, children }) => {
+    const language = className ? className.replace(/language-/, '') : '';
+
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 1500);
+    };
+
+    return (
+      <div style={{ position: 'relative' }}>
+        <CopyToClipboard text={String(children)} onCopy={handleCopy}>
+          <button
+            style={{
+              position: 'absolute',
+              top: '10px',
+              right: '10px',
+              zIndex: '1',
+              padding: '5px',
+              backgroundColor: '#333',
+              color: '#fff',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}
+          >
+            {copied ? 'Copied!' : 'Copy to Clipboard'}
+          </button>
+        </CopyToClipboard>
+        <SyntaxHighlighter
+          style={nightOwl}
+          language={language}
+          PreTag="div"
+          children={String(children).replace(/\n$/, '')} // Remove trailing newline
+        />
+      </div>
+    );
+  },
 };
 
 const BlogPost = ({ source, frontMatter }) => {
   const router = useRouter();
 
   useEffect(() => {
-    // Redirect to home page if a 404 error occurs
+    // Redirect to the home page if a 404 error occurs
     if (!frontMatter) {
       router.replace('/');
     }
@@ -61,20 +104,20 @@ const BlogPost = ({ source, frontMatter }) => {
 export async function getStaticPaths() {
   const BLOGDATA = [
     {
-      id: "1",
-      title: "Test",
+      id: '1',
+      title: 'Test',
     },
     {
-      id: "2",
-      title: "Test 2",
+      id: '2',
+      title: 'Test 2',
     },
     {
-      id: "3",
-      title: "Test 3",
+      id: '3',
+      title: 'Test 3',
     },
     {
-      id: "4",
-      title: "Test 4",
+      id: '4',
+      title: 'Test 4',
     },
   ];
 
