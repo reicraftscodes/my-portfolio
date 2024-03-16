a3e3364b59a158b210294d05dccaae4c1c00dda5
laureisan <reicraftscodes@gmail.com>
2024-03-16 01:42:56 +0000

    refactor: changed portfolio intro

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,8 @@ export default function Home() {
       <section className="w-full flex items-center justify-center">
         <div>
           <p>Hi, my name is</p>
-          <h1 className="font-bold text-3xl ">Laurate S.</h1>
-          <p className="text-right"> Full Stack Software Engineer | UI/UX Designer </p>
+          <h1 className="font-bold text-3xl ">May S.</h1>
+          <p className="text-right"> UI/UX Designer | Developer </p>
           <div className="flex items-center justify-end space-x-4 mt-3">
             {PROFILE_LINKS.map((profileLink) => (
               <a
@@ -46,10 +46,10 @@ export default function Home() {
         <Spacer />
         <h2 className="text-2xl font-bold tracking-widest">/about</h2>
         <p>
-          Hi, I&apos;m Laurate, you can call me May. I&apos;m a a full stack software engineer based in the UK and Cardiff University graduate. I&apos;m proudly deaf, self-motivated, performance-driven, and a detail-oriented person with extensive knowledge of software designs, development, and testing.
+          Hi, I&apos;m May you call me Laurate, I&apos;m based in Cardiff UK and Cardiff University graduate. I&apos;m self-motivated, performance-driven, and a detail-oriented person with extensive knowledge of UI designs and different technological platforms and programming languages.
         </p>
         <p>
-          Currently, I am seeking for a full time Full Stack Software Engineering especially in Java Spring, but I am open to any opportunities and always willing to learn!
+          Currently, I am actively seeking a Software Engineering role, preferably in Java, although I am open to exploring opportunities in other technologies. I am particularly interested in intern or junior developer positions where I can continue to learn and grow.
         </p>
         <p>
           My hobbies include reading novels, playing online/video games, acrylic painting, photography, traveling, and cooking. I like to play drums although my right ear may be deaf, I can still play the drums. Cool right?
